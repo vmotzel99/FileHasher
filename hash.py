0b7a1b2168cda1c50b986b65e5754937da57bd3b
vmotzel99 <60457283+vmotzel99@users.noreply.github.com>
2022-03-24 21:40:21 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,6 +1,20 @@
 import os
 import hashlib
+from datetime import datetime
 
 nohashlist = [/dev, /proc, /run, /sys, tmp, /var/lib, /var/run]
 
-fnames = os.walk
+st = open("hashstored.txt", "a")
+
+for root, dirs, files in os.walk("/"):
+  for filename in files:
+    if root not in nohashlist:
+      print(os.path.join(root, filename))
+      with open(filename, "rb") as f:
+        bytes = f.read()
+        readfhash = hashlib.sha256(bytes).hexdigest();
+        st.write(filename, readfhash, datetime.now())
+      
+    
+        
+
