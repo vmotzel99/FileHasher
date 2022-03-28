be269fcf89021de23f5753122be3f8a08f2d7282
vmotzel99 <60457283+vmotzel99@users.noreply.github.com>
2022-03-27 20:47:48 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,21 +1,30 @@
 import os
+import sys
 import hashlib
 from datetime import datetime
 
-nohashlist = [/dev, /proc, /run, /sys, tmp, /var/lib, /var/run]
+nohashlist = ['dev', 'proc', 'run', 'sys', 'tmp', 'var/lib', 'var/run', 'boot', 'mnt', 'var', 'opt', 'etc']
 
-st = open("hashstored.txt", "a")
+def main():
+    st = open("hashstored.txt", "a")
+    for root, dirs, filen in os.walk('/'):
+        for fils in filen:
+            addpath = (os.path.join(root, fils))
+            if ".cache" in str(addpath) or ".vscode" in str(addpath) or ".mozilla" in str(addpath):
+                break
+            else:
+                for i in nohashlist:
+                    if i not in addpath:
+                        with open(addpath, "rb") as f:
+                            bytes = f.read()
+                            readfhash = hashlib.sha256(bytes).hexdigest()
+                            st.write(fils)
+                            st.write(readfhash)
+                            st.write(str(datetime.now()) + "\n") 
+    st.close()
 
-for root, dirs, files in os.walk("/"):
-  for filename in files:
-    if root not in nohashlist:
-      print(os.path.join(root, filename))
-      with open(filename, "rb") as f:
-        bytes = f.read()
-        readfhash = hashlib.sha256(bytes).hexdigest();
-        st.write(filename, readfhash, datetime.now())
-        
-st.close()
+if __name__=="__main__":
+    main()
       
     
         
